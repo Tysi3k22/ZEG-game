66ca4c85df75f000b03a7ecf3804480587c39bd4
Tysi3k22 <kacpert.kasiak@gmail.com>
2026-04-14 22:13:09 +0200

    zmiana dzialania hp oraz kluczy

--- ZEGgame/source/app.js
+++ ZEGgame/source/app.js
@@ -5,6 +5,16 @@ let hp = document.getElementById('hp');
 let klucze = document.getElementById('klucze');
 
 
+//podstawowe informacje o graczu
+let player = {
+    x: 1,
+    y: 1,
+    keys: 0,
+    hp: 20 
+};
+hp.innerHTML = player.hp + "/100";
+klucze.innerHTML = parseInt(player.keys); 
+
 //Swieder mapowanie masz tak map1[y][x]
 const map1 = [
     [1,1,1,1,1,1,1,1,1,1,1,1],//1 - sciana
@@ -21,13 +31,12 @@ const map1 = [
     [1,1,1,1,1,1,1,1,1,1,1,1]
 ];
 
-//podstawowe informacje o graczu
-let player = {
-    x: 1,
-    y: 1,
-    keys: 0,
-    hp: 100 
-};
+
+function Fog() {
+    ctx.beginPath();
+    ctx.arc(20, 20, 40, 0, 2 * Math.PI);
+    ctx.stroke();
+}
 
 function Draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height); //zresetowanie wszelkich rzeczy w canvasie
@@ -65,7 +74,7 @@ function move(dx, dy) {
     const px = player.x + dx;
     const py = player.y + dy;
     //blokada przed przejściem dalej bez kluczy
-    if (licznikKluczy <= 1 && map1[py][px] == 2) {
+    if (player.keys <= 1 && map1[py][px] == 2) {
         alert("Musisz zdobyć wszystkie klucze, aby przejść dalej!"); //alert pokazujacy ze trzeba zdobyć wszystkie klucze aby przejsc dalej
         return; //zatrzymanie funkcji move, aby nie pozwolić na przejście dalej
     }
@@ -75,7 +84,7 @@ function move(dx, dy) {
         player.x = px;
         player.y = py;
     }
-    if(map1[py][px] == 2 && licznikKluczy == 2 ){ //TODO: blokuje wchodzenie na pole mety
+    if(map1[py][px] == 2 && player.keys == 2 ){ //TODO: blokuje wchodzenie na pole mety
         alert("Wygrałeś!"); //alert pokazujacy przescie labiryntu (trzeba zrobic menu glowne aby do niego przechodzic po skonczeniu)
     }
     if(map1[py][px] == 3){
@@ -86,7 +95,8 @@ function move(dx, dy) {
     }
     if(map1[py][px] == 4){
         alert("Zdobyłeś leczenie!");
-        hp.innerHTML = "100/100";
+        player.hp = 100;
+        hp.innerHTML = player.hp + "/100";
         map1[py][px] = 0; //usuwanie leczenia z mapy po odebraniu
     }
     Draw();
